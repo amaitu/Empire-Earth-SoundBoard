5fbf65cda8f0a11ea6456979ab6d1da8b2e92d07
amaitu <steve@sgbarker.com>
2019-09-29 13:21:10 +0100

    Removed unused CSS.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './src/**/*.html',
+        './public/**/*.html',
+        './src/**/*.vue',
+        './public/**/*.vue',
+    ],
+
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-    ]
+        purgecss,
+    ],
 };
