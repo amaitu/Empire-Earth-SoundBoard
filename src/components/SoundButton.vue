7a85ff64ed187e7231870e799b16ed0140e7d008
amaitu <steve@sgbarker.com>
2019-12-11 18:37:29 +0000

    Sound button hover effects.

--- src/components/SoundButton.vue
+++ src/components/SoundButton.vue
@@ -1,7 +1,7 @@
 <template>
     <div
             v-on:click="activateButton()"
-            class="cursor-pointer font-bold w-1/3 p-3 md:w-1/6 lg:w-1/6 lg:w-1/12 xl:w-1/12 mx-2 xl:mx-3 mb-4 h-20 rounded text-white text-sm text-center bg-black opacity-50 shadow-2xl border-yellow-200">
+            class="cursor-pointer hover:opacity-75 font-bold w-1/3 p-3 md:w-1/6 lg:w-1/6 lg:w-1/12 xl:w-1/12 mx-2 xl:mx-3 mb-4 h-20 rounded text-white text-sm text-center bg-black opacity-50 shadow-2xl border-yellow-200">
         <span>{{text}}</span>
     </div>
 </template>
