42363994c2032cff7254797edf7d12a9444c91a3
amaitu <steve@sgbarker.com>
2020-04-25 15:08:21 +0100

    Add loading indicators.

--- src/components/SoundButton.vue
+++ src/components/SoundButton.vue
@@ -2,13 +2,21 @@
     <div
             v-on:click="activateButton()"
             class="cursor-pointer hover:opacity-75 font-bold w-1/3 p-3 md:w-1/6 lg:w-1/6 lg:w-1/12 xl:w-1/12 mx-2 xl:mx-3 mb-4 h-24 rounded text-white text-sm md:text-base lg:text-sm text-center bg-black opacity-50 shadow-2xl border-yellow-200">
-        <span>{{buttonCopy}}</span>
+        <span v-if="playState === 'default'">
+            {{buttonCopy}}
+        </span>
+        <span v-if="playState === 'active'">
+            <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        </span>
     </div>
 </template>
 
 <script>
     import {groups, groupTypes} from '@/groups';
     import {getStorageName} from "@/utils/utils.js";
+
+    const PLAY_STATE_DEFAULT = 'default';
+    const PLAY_STATE_ACTIVE = 'active';
 
     export default {
         name: 'SoundButton',
@@ -23,38 +31,55 @@
             return {
                 audio: null,
                 buttonCopy: this.text,
+                playState: PLAY_STATE_DEFAULT,
             }
         },
 
         methods: {
             activateButton: function () {
-                if (this.buttonCopy === 'stop' && this.audio) {
-                    this.pauseAudio();
-                    this.buttonCopy = this.text;
+                // If the audio is already playing, return.
+                if (this.playState === PLAY_STATE_ACTIVE) {
                     return;
                 }
 
                 if (this.type === 'random') {
-                    this.playRandom();
-                    return;
+                    return this.playRandom();
                 }
 
-                const path = this.getPath(this.type, this.filename, this.text);
-
-                if (this.type === 'music') {
-                    this.buttonCopy = 'stop';
-                }
-
-                this.playAudio(path);
+                this.playState = PLAY_STATE_ACTIVE;
+                this.playAudio(
+                    this.getPath(this.type, this.filename, this.text)
+                ).then(this.resetButton)
             },
 
-            pauseAudio: function () {
-                this.audio.pause();
+            resetButton: function () {
+                this.playState = 'default'
             },
+
+            // isPausable: function () {
+            // todo - make music sounds pausable
+            // return this.type === 'music'
+            // },
+
+            // pauseAudio: function () {
+            //     this.audio.pause();
+            // },
 
             playAudio: function (path) {
+                return new Promise(function (resolve, reject) {   // return a promise
+                    var audio = new Audio(path);                     // create audio wo/ src
+                    audio.preload = "auto";                      // intend to play through
+                    audio.autoplay = true;                       // autoplay when loaded
+                    audio.onerror = reject;                      // on error, reject
+                    audio.onended = resolve;                     // when done, resolve
+
+                    // audio.src = path + url + suffix; // just for example
+                });
+
+            },
+
+            loadAudio: function (path) {
                 this.audio = new Audio(path);
-                this.audio.play();
             },
 
             getPath: function (type, filename, text) {
