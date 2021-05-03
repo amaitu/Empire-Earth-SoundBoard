fc02d04c5d1ceb3b3777b7877e3a0035c30b1893
amaitu <steve@sgbarker.com>
2021-05-03 16:42:17 +0100

    Fix chevron styling

--- src/components/Subheading.vue
+++ src/components/Subheading.vue
@@ -1,7 +1,7 @@
 <template>
   <div
       class="flex tracking-wide uppercase justify-center slide-up capitalize cursor-pointer text-xl lg:text-2xl hover:text-empire-red-light text-empire-red mb-4 text-center mt-10 lg:mt-12">
-    <h2> {{ text }} </h2> <span v-bind:class="{ 'upside-down': !hidden }" class="mx-2"> <img style="width: 18px" class="select-none hover:-rotate-180 bg-empire-red-light" alt="expand section" src="../assets/img/chevron-arrow-down.svg"></span>
+    <h2> {{ text }} </h2> <span v-bind:class="{ 'upside-down': !hidden }" class="mx-2"> <img style="width: 18px; margin-top: 8px;" class="select-none" alt="expand section" src="../assets/img/chevron-arrow-down.svg"></span>
   </div>
 </template>
 
