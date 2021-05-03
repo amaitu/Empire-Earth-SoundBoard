9e4e3ea75a22bca97d8a9332f8e504d82fc71ddf
amaitu <steve@sgbarker.com>
2021-05-03 16:48:38 +0100

    Prerender

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+const path = require('path');
+const Renderer = PrerenderSpaPlugin.PuppeteerRenderer;
+
+module.exports = {
+    configureWebpack: config => {
+        if (process.env.NODE_ENV !== 'production') return
+
+        return {
+            plugins: [
+                new PrerenderSpaPlugin({
+                    // Absolute path to compiled SPA
+                    staticDir: path.resolve(__dirname, 'dist'),
+                    // routes that should be pre-rendered:
+                    routes: [
+                        '/',
+                    ],
+
+                    renderer: new Renderer({
+                        injectProperty: '__PRERENDER_INJECTED',
+                        inject: {
+                            prerendered: true
+                        },
+                        renderAfterDocumentEvent: 'app.rendered'
+                    })
+                })
+            ]
+        }
+    }
+}
